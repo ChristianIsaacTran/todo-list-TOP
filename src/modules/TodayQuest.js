74af899a472a00993d40921cb7c9d5a95a7aa9a8
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-04-30 16:32:44 -0500

    Made today's quest webpage in raw HTML
    I also added stylings to the today's quest
    html. I will have to add functionality
    in javascript for the buttons, but the basic look
    of the page is correct.

---
+++ src/modules/TodayQuest.js
@@ -0,0 +1,13 @@
+import ProjectAndTodo from "./ProjectAndTodo.js";
+
+//Main homepage module. A summary of the current project selected
+//Generate Today Quest content
+
+function TodayQuest() {
+    
+    function generateTodayQuest() {
+        const contentContainer = document.querySelector("#content");
+    }
+}
+
+export default TodayQuest;
