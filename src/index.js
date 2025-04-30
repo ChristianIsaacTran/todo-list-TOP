3f1c74f7cf72cdff175c936b1bc39c3f3bcb6577
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-04-30 15:39:01 -0500

    created different tab modules
    Make sure to work on each page as a separate
    entity so that it doesn't create dependencies
    between the modules. Remember, single
    responsibility.

--- src/index.js
+++ src/index.js
@@ -6,20 +6,7 @@ import  ProjectAndTodo from "./modules/ProjectAndTodo.js";
 
 //Menu switching logic will go here.
 
-const appManage = ProjectAndTodo();
+// const appManage = ProjectAndTodo();
 
-// appManage.createProject("new project 1");
-// appManage.createTodo("do laundry 3", "I have to do laundry", "1/1/1", "high", "new project 1");
 
-// appManage.removeTodo("fwefw", "new project 1");
-
-// appManage.updateProjectName("new project 1", "new project 2");
-
-// appManage.getProject("new project 1");
-
-// appManage.getTodo("do laundry 2", "new project 2");
-
-// appManage.updateTodo("do laundry 2", "new project 2", "new task", "new task", "new task", "new task");
-
-// appManage.viewProject("new project 2");
 
