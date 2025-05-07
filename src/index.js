8b1882727a1a26b5329045965f4a42f35026ea46
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-05-07 12:50:35 -0500

    Css stylings for todo overflow, fixed btn pointer
    Made the todo container handle vertical overflow
    and also made it so that if it does overflow,
    the button pointer finger follows the buttons
    relative to their positioning with the scroll
    element. Put the absolute positioned button
    pointer into a relative position html element.

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const todayQuestPage = TodayQuest();
 // appManage.createProject("project 1");
 // appManage.createProject("project 2");
 
-// appManage.createTodo("todo 1", "fewwe","fewewff","fwefweew","incomplete", "project 1");
-// appManage.createTodo("todo 2", "fewwe","fewewff","fwefweew","incomplete", "project 1");
-// appManage.createTodo("todo 4", "this is another description.","01/01/20","high","incomplete", "project 2");
+// appManage.createTodo("todo 1", "fewwe","fewewff","fwefweew","incomplete", "Default Project");
+// appManage.createTodo("todo 2", "fewwe","fewewff","fwefweew","incomplete", "Default Project");
+// appManage.createTodo("todo 4", "this is another description.","01/01/20","high","incomplete", "Default Project");
 
 //First time render, or page open render
 todayQuestPage.generateTodayQuest();
