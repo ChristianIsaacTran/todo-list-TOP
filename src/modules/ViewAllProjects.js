482f7a503c18cb5eef51af60893fc2835903eb3a
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-05-08 12:56:07 -0500

    Added slight tweaks to the alignment viewAllProj

---
+++ src/modules/ViewAllProjects.js
@@ -0,0 +1,12 @@
+import ProjectAndTodo from "./ProjectAndTodo.js";
+
+function ViewAllProjects() {
+
+    function generateViewAllProjects() {
+        
+    }
+
+    return {generateViewAllProjects};
+}
+
+export default ViewAllProjects;
